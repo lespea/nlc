e91707c8c133aaa4652e8eb493b3c69b46ba6616
Adam Lesperance <lespea@gmail.com>
2019-04-21 13:15:34 -0500

    Get the num formatter working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use num_format::{Buffer, Locale};
+use std::io;
+
+fn print(total: &u64) {
+    let mut buf = Buffer::default();
+    buf.write_formatted(total, &Locale::en);
+    println!("{}", buf)
+}
+
+fn main() -> io::Result<()> {
+    let n = 123478u64;
+
+    print(&n);
+    Ok(())
 }
